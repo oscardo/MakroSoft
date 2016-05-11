7c4ea045f3aae74ed0735fe54d417a32d1f9557f
Oscar Eduardo Ortiz Pinzon <oscardo2000@hotmail.com>
2016-05-11 17:26:37 -0500

    1.

--- Web/WebMakroSoft/WebMakroSoft/Administracion/Parametros/Usuario/Usuario.aspx.designer.cs
+++ Web/WebMakroSoft/WebMakroSoft/Administracion/Parametros/Usuario/Usuario.aspx.designer.cs
@@ -11,5 +11,23 @@ namespace WebMakroSoft.Administracion.Parametros.Usuario {
     
     
     public partial class Usuario {
+        
+        /// <summary>
+        /// GridView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+        
+        /// <summary>
+        /// EntityDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.EntityDataSource EntityDataSource1;
     }
 }
